a3efc40593617c8d1614c671293fde789d96a8d3
Nimanivu <nimajoshy2004@gmail.com>
2025-04-04 16:14:18 +0530

    Update Priorityqueue.java

--- Priorityqueue.java
+++ Priorityqueue.java
@@ -10,6 +10,14 @@ class Priorityqueue {
         qu.offer(29);    
         qu.offer(67);    
         
+       System.out.println("Priority queue:"+qu);
+       System.out.println("peek element:" +qu.peek());
+       System.out.println("element removed is:" +qu.remove());
+       System.out.println("size of the priority queue:" +qu.size());
+       System.out.println("check the  priority queue empty: " +qu.isEmpty());
+       System.out.println("Whether queue contains 20:" +qu.contains(20));
+       qu.clear();
        System.out.println(qu);
+
        }
        }
