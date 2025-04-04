7e4ebd46838599f207eb2845abb81b16e89127e7
Nimanivu <nimajoshy2004@gmail.com>
2025-04-04 11:21:56 +0530

    Add files via upload

---
+++ Priorityqueue.java
@@ -0,0 +1,15 @@
+import java.util.*;    
+class Priorityqueue {    
+    public static void main(String args[]) {    
+        // Creating a PriorityQueue of Strings  
+        PriorityQueue<Integer> qu = new PriorityQueue<Integer>();    
+        // Adding elements to the PriorityQueue  
+        qu.add(39);    
+        qu.add(19);    
+        qu.add(89);    
+        qu.offer(29);    
+        qu.offer(67);    
+        
+       System.out.println(qu);
+       }
+       }
